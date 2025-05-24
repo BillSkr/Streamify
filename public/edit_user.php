d1ad9fad6dc5a2261c67993fc26e33e26e9ab7bd
filipposbitop <115323068+filipposbitop@users.noreply.github.com>
2025-05-24 17:19:38 +0300

    Add files via upload

--- public/edit_user.php
+++ public/edit_user.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $conn = new mysqli("localhost", "username", "password", "database");
 
 if ($conn->connect_error) {
